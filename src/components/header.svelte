1022cd9a7a0c3d776ced35cbcbbcc892699559ee
AbbanFahim <lloydtechno58@gmail.com>
2022-10-05 19:32:00 +0400

    initial layout and styling

---
+++ src/components/header.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { onMount } from "svelte";
+  export let selector;
+
+  let headerRef;
+  function throttle(fn, wait) {
+    var time = Date.now();
+    return function () {
+      if (time + wait - Date.now() < 0) {
+        fn();
+        time = Date.now();
+      }
+    };
+  }
+  onMount(() => {
+    const elementToScrollBy = document.querySelector(selector);
+    document.addEventListener(
+      "scroll",
+      throttle((e) => {
+        if (document.documentElement.scrollTop > elementToScrollBy.clientHeight) {
+          headerRef.classList = "visible";
+        } else {
+          headerRef.classList = "";
+        }
+      }, 100)
+    );
+  });
+</script>
+
+<header bind:this={headerRef}>lol</header>
